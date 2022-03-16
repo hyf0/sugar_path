003e46f61153dc866b740a72221c17ac53c37c8c
Yunfei He <i.heyunfei@gmail.com>
2022-03-17 01:12:56 +0800

    Fix normalize for windows

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,4 @@
-use std::{
-    ffi::OsStr,
-    os::unix::prelude::OsStrExt,
-    path::{Component, Path, PathBuf},
-};
+use std::path::{Component, Path, PathBuf};
 
 use once_cell::sync::Lazy;
 
@@ -17,56 +13,48 @@ pub trait PathSugar {
 }
 
 #[inline]
-fn normalize_to_component_vec(mut path: &Path) -> Vec<Component> {
-    let normalize = |p: &Path| {
-        let mut components = path.components().peekable();
-        let mut ret = if let Some(c @ Component::Prefix(..)) = components.peek().cloned() {
-            components.next();
-            vec![c]
-        } else {
-            vec![]
-        };
+fn normalize_to_component_vec(path: &Path) -> Vec<Component> {
+    let mut components = path.components().peekable();
+    let mut ret = if let Some(c @ Component::Prefix(..)) = components.peek().cloned() {
+        components.next();
+        vec![c]
+    } else {
+        vec![]
+    };
 
-        for component in components {
-            match component {
-                Component::Prefix(..) => unreachable!(),
-                Component::RootDir => {
-                    ret.push(component);
-                }
-                Component::CurDir => {}
-                c @ Component::ParentDir => {
-                    let is_last_none = ret.last().is_none();
-                    if is_last_none {
-                        ret.push(c);
+    for component in components {
+        match component {
+            Component::Prefix(..) => unreachable!(),
+            Component::RootDir => {
+                ret.push(component);
+            }
+            Component::CurDir => {}
+            c @ Component::ParentDir => {
+                let is_last_none = ret.last().is_none();
+                if is_last_none {
+                    ret.push(c);
+                } else {
+                    let is_last_root = matches!(ret.last().unwrap(), Component::RootDir);
+                    if is_last_root {
+                        // do nothing
                     } else {
-                        let is_last_root = matches!(ret.last().unwrap(), Component::RootDir);
-                        if is_last_root {
-                            // do nothing
+                        let is_last_parent_dir =
+                            matches!(ret.last().unwrap(), Component::ParentDir);
+                        if is_last_parent_dir {
+                            ret.push(c);
                         } else {
-                            let is_last_parent_dir =
-                                matches!(ret.last().unwrap(), Component::ParentDir);
-                            if is_last_parent_dir {
-                                ret.push(c);
-                            } else {
-                                ret.pop();
-                            }
+                            ret.pop();
                         }
                     }
                 }
-                c @ Component::Normal(_) => {
-                    ret.push(c);
-                }
+            }
+            c @ Component::Normal(_) => {
+                ret.push(c);
             }
         }
-
-        ret
-    };
-    if cfg!(target_family = "windows") {
-        let safe = PathBuf::from(path.to_string_lossy().to_string().replace("/", "\\"));
-        normalize(&safe)
-    } else {
-        normalize(path)
     }
+
+    ret
 }
 
 #[inline]
@@ -82,8 +70,14 @@ fn component_vec_to_path_buf(components: Vec<Component>) -> PathBuf {
 
 impl PathSugar for Path {
     fn normalize(&self) -> PathBuf {
-        let components = normalize_to_component_vec(self);
-        component_vec_to_path_buf(components)
+        if cfg!(target_family = "windows") {
+            let safe = PathBuf::from(self.to_string_lossy().to_string().replace("/", "\\"));
+            let components = normalize_to_component_vec(&safe);
+            component_vec_to_path_buf(components)
+        } else {
+            let components = normalize_to_component_vec(self);
+            component_vec_to_path_buf(components)
+        }
     }
     fn resolve(&self) -> PathBuf {
         let mut components = self.components().peekable();
