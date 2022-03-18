e840f057f2f0f3179c708c8d1963098d43091bef
Yunfei He <i.heyunfei@gmail.com>
2022-03-19 00:40:38 +0800

    Fix

--- tests/resolve.rs
+++ tests/resolve.rs
@@ -49,18 +49,18 @@ fn unix() {
 fn windows() {
     assert_eq!(path_buf!("c:../a").resolve(), path_buf!("c:\\a"));
     assert_eq!(path_buf!("a").resolve(), get_cwd().join("a"));
-    assert_eq!(path_buf!("."), get_cwd());
-    assert_eq!(path_buf!(""), get_cwd());
-    assert_eq!(path_buf!("../../../foo/../../../bar"), get_cwd().push("bar"));
-    assert_eq!(path_buf!("c:/ignore"), path_buf!("c:\\ignore"));
-    assert_eq!(path_buf!("c:\\some\\file"), path_buf!("c:\\some\\file"));
-    assert_eq!(path_buf!("some/dir//"), get_cwd().join("some").join("dir"));
+    assert_eq!(path_buf!(".").resolve(), get_cwd());
+    assert_eq!(path_buf!("").resolve(), get_cwd());
+    assert_eq!(path_buf!("../../../foo/../../../bar").resolve(), get_cwd().push("bar"));
+    assert_eq!(path_buf!("c:/ignore").resolve(), path_buf!("c:\\ignore"));
+    assert_eq!(path_buf!("c:\\some\\file").resolve(), path_buf!("c:\\some\\file"));
+    assert_eq!(path_buf!("some/dir//").resolve(), get_cwd().join("some").join("dir"));
     assert_eq!(
-        path_buf!("//server/share", "..", "relative\\"),
+        path_buf!("//server/share", "..", "relative\\").resolve(),
         get_cwd().join(path_buf!("\\\\server\\share\\relative"))
     );
     assert_eq!(
-        path_buf!("..\\tmp.3\\cycles\\root.js"),
+        path_buf!("..\\tmp.3\\cycles\\root.js").resolve(),
         get_cwd().join(path_buf!("tmp.3\\cycles\\root.js"))
     );
 }
