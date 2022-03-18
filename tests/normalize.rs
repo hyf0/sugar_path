77e06d2424bd3db5dc228e259a02e3669aacdb4a
Yunfei He <i.heyunfei@gmail.com>
2022-03-18 15:39:53 +0800

    Fix

--- tests/normalize.rs
+++ tests/normalize.rs
@@ -101,7 +101,7 @@ fn windows() {
         Path::new("\\x\\y\\z")
     );
     assert_eq!(Path::new("C:").normalize(), Path::new("C:"));
-    assert_eq!(Path::new("C:..\\abc").normalize(), Path::new("C:\\abc"));
+    assert_eq!(Path::new("C:..\\abc").normalize(), Path::new("C:abc"));
     assert_eq!(Path::new("c:../a"), Path::new("c:\\a"));
     assert_eq!(
         Path::new("C:..\\..\\abc\\..\\def").normalize(),
