133559e4a4d21bd8e3d289275d0b67a6280be71d
YunfeiHe <i.heyunfei@gmail.com>
2022-09-26 16:34:51 +0800

    Improve docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+//! Sugar functions for manipulating paths.
+//! 
+//! [![document](https://docs.rs/sugar_path/badge.svg)](https://docs.rs/crate/sugar_path)
+//! [![crate version](https://img.shields.io/crates/v/sugar_path.svg)](https://crates.io/crates/sugar_path) 
+//! [![MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+//! 
+//! 
+//! - [Examples](https://github.com/iheyunfei/sugar_path/tree/main/tests)
+//! - [Usages](https://docs.rs/sugar_path/latest/sugar_path/trait.SugarPath.html)
+
 use std::{
     path::{Component, Path, PathBuf},
 };
