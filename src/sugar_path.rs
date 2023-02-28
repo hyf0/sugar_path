e15262d4b51c2997ca100baf47802ae1943cce52
HeYunfei <i.heyunfei@gmail.com>
2023-02-28 12:48:04 +0800

    Fix normalize (#17)

--- src/sugar_path.rs
+++ src/sugar_path.rs
@@ -71,22 +71,18 @@ pub trait SugarPath {
 
 impl SugarPath for Path {
     fn normalize(&self) -> Cow<Path> {
-        let components = normalize_to_component_vec(self);
-        if let Some(mut components) = components {
-            if components.is_empty() {
-                return Cow::Borrowed(Path::new("."));
-            }
-
-            if cfg!(target_family = "windows") {
-                if components.len() == 1 && matches!(components[0], Component::Prefix(_)) {
-                    components.push(Component::CurDir)
-                }
-            }
-
-            component_vec_to_path_buf(components).into()
-        } else {
-            Cow::Borrowed(self)
+        let mut components = normalize_to_component_vec(self);
+        if components.is_empty() {
+            return Cow::Borrowed(Path::new("."));
         }
+
+        if cfg!(target_family = "windows") {
+            if components.len() == 1 && matches!(components[0], Component::Prefix(_)) {
+                components.push(Component::CurDir)
+            }
+        }
+
+        component_vec_to_path_buf(components).into()
     }
 
     fn absolutize(&self) -> Cow<Path> {
--- src/sugar_path_buf.rs
+++ src/sugar_path_buf.rs
@@ -17,23 +17,19 @@ pub trait SugarPathBuf {
 
 impl SugarPathBuf for PathBuf {
     fn into_normalize(self) -> PathBuf {
-        let components = normalize_to_component_vec(&self);
+        let mut components = normalize_to_component_vec(&self);
 
-        if let Some(mut components) = components {
-            if components.is_empty() {
-                return PathBuf::from(".");
-            }
-
-            if cfg!(target_family = "windows") {
-                if components.len() == 1 && matches!(components[0], Component::Prefix(_)) {
-                    components.push(Component::CurDir)
-                }
-            }
-
-            component_vec_to_path_buf(components)
-        } else {
-            self
+        if components.is_empty() {
+            return PathBuf::from(".");
         }
+
+        if cfg!(target_family = "windows") {
+            if components.len() == 1 && matches!(components[0], Component::Prefix(_)) {
+                components.push(Component::CurDir)
+            }
+        }
+
+        component_vec_to_path_buf(components)
     }
 
     /// If there's no absolutization to be done, this function will return the original `PathBuf`.
