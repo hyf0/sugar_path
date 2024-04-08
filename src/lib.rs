1544fdafca60dd513a201f941e4e24f43e6d58b5
Yunfei <i.heyunfei@gmail.com>
2024-04-08 18:49:48 +0800

    Refine documents

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,72 @@
-//! Sugar functions for manipulating paths.
-//! 
+//! `SugarPath` is a trait that provides a sugar way to manipulate path.
+//!
 //! [![document](https://docs.rs/sugar_path/badge.svg)](https://docs.rs/crate/sugar_path)
-//! [![crate version](https://img.shields.io/crates/v/sugar_path.svg)](https://crates.io/crates/sugar_path) 
+//! [![crate version](https://img.shields.io/crates/v/sugar_path.svg)](https://crates.io/crates/sugar_path)
 //! [![MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+//!
+//! ## Main functionalities
+//! - [SugarPath::as_path] makes it easy to convert `T: Deref<Target = str>` to [Path](std::path::Path) and allows to you methods of [SugarPath] on `&str` or `String` directly.
+//!
+//! ```rust
+//! use std::path::Path;
+//! use sugar_path::SugarPath;
+//!
+//! assert_eq!("foo".as_path().join("bar"), Path::new("foo/bar"));
+//! assert_eq!("foo/./bar/../baz".normalize(), "foo/baz".as_path());
+//! ```
+//!
+//! - [SugarPath::to_slash]/[SugarPath::to_slash_lossy] allows you to convert the path to the string with consistent slash separator on all platforms.
+//!
+//! ```rust
+//! use sugar_path::SugarPath;
+//!
+//! #[cfg(target_family = "unix")]
+//! let p = "./hello/world".as_path();
+//!
+//! #[cfg(target_family = "windows")]
+//! let p = ".\\hello\\world".as_path();
+//!
+//! assert_eq!(p.to_slash().unwrap(), "./hello/world");
+//! assert_eq!(p.to_slash_lossy(), "./hello/world");
+//! ```
+//! - [SugarPath::normalize] allows you normalize given path by dropping unnecessary `.` or `..` segments.
+//!
+//! ```rust
+//! use std::path::Path;
+//! use sugar_path::SugarPath;
+//!
+//! assert_eq!("foo/./bar/../baz".normalize(), "foo/baz".as_path());
+//! ```
+//!
+//! - [SugarPath::relative] allows you to get the relative path from the given path to the target path.
+//!
+//! ```rust
+//! use sugar_path::SugarPath;
+//!
+//! assert_eq!("/base".relative("/base/project"), "..".as_path());
+//! assert_eq!("/base".relative("/var/lib"), "../../base".as_path());
+//! ```
+//!
+//! - [SugarPath::absolutize] is a shortcut of [SugarPath::absolutize_with] with passing `std::env::current_dir().unwrap()` as the base path.
 //! 
+//! ```rust
+//! use sugar_path::SugarPath;
+//! let cwd = std::env::current_dir().unwrap();
+//! assert_eq!("hello/world".absolutize(), cwd.join("hello").join("world"));
+//! ```
 //! 
-//! - [Examples](https://github.com/iheyunfei/sugar_path/tree/main/tests)
-//! - [Usages](https://docs.rs/sugar_path/latest/sugar_path/trait.SugarPath.html)
+//! - [SugarPath::absolutize_with] allows you to absolutize the given path with the base path.
+//!
+//! ```rust
+//! use sugar_path::SugarPath;
+//!
+//! assert_eq!("./world".absolutize_with("/hello"), "/hello/world".as_path());
+//! assert_eq!("../world".absolutize_with("/hello"), "/world".as_path());
+//! ```
+//!
+//! - For more details, please refer to the [SugarPath].
 
+mod impl_sugar_path;
 mod sugar_path;
 mod utils;
-mod impl_sugar_path;
-
 pub use sugar_path::SugarPath;
--- src/sugar_path.rs
+++ src/sugar_path.rs
@@ -3,11 +3,12 @@ use std::{
   path::{Path, PathBuf},
 };
 
-
 pub trait SugarPath {
   /// Normalizes the given path, resolving `'..'` and `'.'` segments.
   ///
   /// If normalized path is empty, `'.'` is returned, representing the current working directory.
+  ///
+  /// ## Examples
   ///
   /// ```rust
   /// use std::path::Path;
@@ -16,8 +17,8 @@ pub trait SugarPath {
   /// // For example, on POSIX:
   /// #[cfg(target_family = "unix")]
   /// assert_eq!(
-  ///   Path::new("/foo/bar//baz/asdf/quux/..").normalize().to_slash_lossy(),
-  ///   "/foo/bar/baz/asdf"
+  ///   Path::new("/foo/bar//baz/asdf/quux/..").normalize(),
+  ///   Path::new("/foo/bar/baz/asdf")
   /// );
   ///
   /// // On Windows:
@@ -36,14 +37,26 @@ pub trait SugarPath {
   /// ```
   fn normalize(&self) -> PathBuf;
 
-  /// Shortcut for `self.absolutize_with(std::env::current_dir().unwrap())`
+  /// A shortcut of [SugarPath::absolutize_with] with passing `std::env::current_dir().unwrap()` as the base path.
   ///
-  /// See [SugarPath::absolutize_with] for more details.
+  /// ## Examples
+  /// 
+  /// ```rust
+  /// use sugar_path::SugarPath;
+  /// let cwd = std::env::current_dir().unwrap();
+  /// assert_eq!("hello/world".absolutize(), cwd.join("hello").join("world"));
+  /// ```
   fn absolutize(&self) -> PathBuf;
 
-  /// If the given path is absolute, call [SugarPath::normalize] and return it.
+  /// Allows you to absolutize the given path with the base path.
   ///
-  /// If the give path is not absolute, it would be joined with the base path, then normalize and return it.
+  /// ## Examples
+  /// 
+  /// ```rust
+  /// use sugar_path::SugarPath;
+  /// assert_eq!("./world".absolutize_with("/hello"), "/hello/world".as_path());
+  /// assert_eq!("../world".absolutize_with("/hello"), "/world".as_path());
+  /// ```
   fn absolutize_with(&self, base: impl Into<PathBuf>) -> PathBuf;
 
   ///
@@ -51,82 +64,77 @@ pub trait SugarPath {
   /// use std::path::Path;
   /// use sugar_path::SugarPath;
   /// assert_eq!(
-  ///   Path::new("/var").relative("/var/lib").to_slash_lossy(),
-  ///   ".."
+  ///   Path::new("/base").relative("/base/lib"),
+  ///   Path::new("..")
   /// );
   /// assert_eq!(
-  ///   Path::new("/bin").relative("/var/lib").to_slash_lossy(),
-  ///   "../../bin"
+  ///   Path::new("/base").relative("/var/lib"),
+  ///   Path::new("../../base")
   /// );
   /// assert_eq!(
-  ///   Path::new("/a/b/c/d").relative("/a/b/f/g").to_slash_lossy(),
-  ///   "../../c/d"
+  ///   Path::new("/a/b/c/d").relative("/a/b/f/g"),
+  ///   Path::new("../../c/d")
   /// );
   /// ```
   fn relative(&self, to: impl AsRef<Path>) -> PathBuf;
 
   /// [SugarPath::to_slash] converts the path to a string and replaces each separator character with a slash ('/').
-  /// 
+  ///
   /// ## Examples
-  /// 
+  ///
   /// ```rust
   /// use std::path::Path;
   /// use sugar_path::SugarPath;
-  /// 
+  ///
   /// #[cfg(target_family = "unix")]
   /// let p = Path::new("./hello/world");
-  /// 
+  ///
   /// #[cfg(target_family = "windows")]
   /// let p = Path::new(".\\hello\\world");
-  /// 
+  ///
   /// assert_eq!(p.to_slash().unwrap(), "./hello/world");
   /// ```
-  /// 
+  ///
   /// ## In Depth
-  /// 
+  ///
   /// When you convert [Path] to [String], you might get different results on different platforms. For `Path::new("./hello/world")`,
   /// you will get `"./hello/world"` on Unix-like systems and `".\\hello\\world"` on Windows. This especially becomes a problem when
   /// your snapshot files of tests contains paths.
-  /// 
+  ///
   /// This method solves this problem by converting the path to a string and replacing each separator character with a slash ('/').
   /// So for `Path::new("./hello/world")`, you will get `"./hello/world"` on both Unix-like systems and Windows.
-  /// 
+  ///
   /// [SugarPath::to_slash] use [Path::to_str] to convert the path to string under the hood, so it will return `None` if the path contains invalid UTF-8.
   fn to_slash(&self) -> Option<Cow<str>>;
 
-  /// See [SugarPath::to_slash]
-  /// 
+  /// This method is similar to [SugarPath::to_slash], but it use [Path::to_string_lossy] to convert the path to string.
+  ///
   /// ## Examples
-  /// 
+  ///
   /// ```rust
   /// use std::path::Path;
   /// use sugar_path::SugarPath;
-  /// 
+  ///
   /// #[cfg(target_family = "unix")]
   /// let p = Path::new("./hello/world");
-  /// 
+  ///
   /// #[cfg(target_family = "windows")]
   /// let p = Path::new(".\\hello\\world");
-  /// 
+  ///
   /// assert_eq!(p.to_slash_lossy(), "./hello/world");
   /// ```
-  /// 
-  /// ## In Depth
-  /// 
-  /// This method is similar to [SugarPath::to_slash], but it use [Path::to_string_lossy] to convert the path to string.
   fn to_slash_lossy(&self) -> Cow<str>;
 
-  /// An utility method to convert the type to [Path]. This will alow you manipulate the path more easily.
-  /// 
+  /// An utility method to makes it easy to convert `T: Deref<Target = str>` to [Path](std::path::Path) and allows to you methods of [SugarPath] on `&str` or `String` directly.
+  ///
   /// ## Examples
-  /// 
+  ///
   /// ```rust
+  /// use std::path::Path;
   /// use sugar_path::SugarPath;
-  /// 
-  /// assert_eq!(
-  ///   "hello/.".as_path().join("world").normalize().to_slash_lossy(),
-  ///   "hello/world"
-  /// );
+  ///
+  /// assert_eq!("foo".as_path().join("bar"), Path::new("foo/bar"));
+  /// assert_eq!("foo/./bar/../baz".normalize(), "foo/baz".as_path());
   /// ```
   fn as_path(&self) -> &Path;
 }
